4277434fdf57cfff03287aafbd175385f5b0e3d9
Michael Hu <pxmyhdev@gmail.com>
2022-05-14 01:13:24 -0400

    add hydro

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from gas import gas_payment_schedule
 from internet import internet_payment_schedule
 from garbage import garbage_collection_schedule
+from hydro import hydro_payment_schedule
+from utility import utility_payment_schedule
 
 app = Flask(__name__)
 
@@ -10,16 +12,28 @@ def schedules():
     gas = gas_payment_schedule()
     internet = internet_payment_schedule()
     waste_collection = garbage_collection_schedule()
+    hydro = hydro_payment_schedule()
+    utility = utility_payment_schedule()
     
     
-    response_header = """
+    internet_header = """
     <header>
         <h1>Internet</h1>
     </header>
     """
     print(f"internet = {internet}")
-    response_body = ""
+    internet_body = ""
     for date,amount in internet.items():
-        response_body += f"<p>{date}: {amount}</p>"
+        internet_body += f"<p>{date}: {amount}</p>"
+
+    hydro_header = """
+    <header>
+        <h1>Hydro</h1>
+    </header>
+    """
+    print(f"hydro = {hydro}")
+    hydro_body = ""
+    for date,amount in hydro.items():
+        hydro_body += f"<p>{date}: {amount}</p>"
     
-    return response_header + response_body
+    return internet_header + internet_body + hydro_header + hydro_body
--- internet.py
+++ internet.py
@@ -2,6 +2,7 @@ import secrets
 
 
 def internet_payment_schedule():
+    # billing date is always 23rd of each month, need to figure out when bill due date is calculated
     schedule = {
         "2022-03-22": "161.79",
         "2022-04-19": "51.97",
